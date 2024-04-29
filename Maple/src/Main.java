af37f996ca1f76966980b2b4a453f197354b583c
Henry To <144722748+tigy1@users.noreply.github.com>
2024-04-29 10:48:26 -0700

    Update Main.java lol

--- Maple/src/Main.java
+++ Maple/src/Main.java
@@ -2,6 +2,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -12,10 +13,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.Timer;
 
 public class Main extends JPanel implements ActionListener, MouseListener, KeyListener {
@@ -29,34 +37,94 @@ public class Main extends JPanel implements ActionListener, MouseListener, KeyLi
     double minLat = -85.05112878;
     int width = 1280;
     int height = 641;
+    JButton button;
+    JTextField textField;
+    JLabel map;
     
     double xScale = width/(maxLong - minLong);
     double yScale = height/(maxLat - minLat);
     
     //sets background
     private ImageIcon backgroundImage = new ImageIcon("Map.jpg");
-
+    private static Coordinate inputted;
+    private static String in = "Russian Federation";
     public static void main(String[] arg) {
-        new Main();
+        new Main(); 
+        readInput();
+    }
+
+    public static void readInput(){
+        try {
+			Scanner scanner = new Scanner(new File("Centroids.csv"));
+			
+			if (scanner.hasNextLine()) {
+                scanner.nextLine(); // Skip the first line
+            }
+			
+			HashMap country = new HashMap<String, Coordinate>();
+			
+			 while (scanner.hasNextLine()) {
+	    			String[] data = scanner.nextLine().split(",");
+	                for(int i = 2; i < data.length; i++) {
+	                	System.out.println(data[i]);
+	                	i+=2;
+	                }
+	                double latitude = Double.parseDouble(data[0]);
+	                double longitude = Double.parseDouble(data[1]);
+	                String c = data[2];
+	                country.put(c, new Coordinate(latitude, longitude));
+	            }
+//			 while(true) {
+//				Scanner input = new Scanner(System.in);
+//				System.out.println("input country");
+//                in = input.nextLine();
+//                inputted = (Coordinate)(country.get(in));
+//			}
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}
     }
     
     //frame constructor
     public Main() {
-        JFrame f = new JFrame("Map");
-        f.setLayout(new BorderLayout());
-        f.setSize(new Dimension(width, height));
-        f.setResizable(false);
-        f.addMouseListener(this);
-        f.addKeyListener(this);
+        JFrame world = new JFrame("Map");
+        JFrame text = new JFrame("Text");
+        textField = new JTextField(20);
+        button = new JButton("Enter");
+        button.addActionListener(this);
+        text.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        text.setLayout(new FlowLayout());
+		text.setResizable(false);
+		text.pack();
+        // Add the JTextField to the JFrame
+        
+        text.add(textField);
+
+        // Set the size of the JFrame
+        text.setSize(300, 100);
+
+        // Make the JFrame visible
+        text.add(button);
+        text.setVisible(true);
+        world.setLayout(new BorderLayout());
+        world.setSize(new Dimension(width, height));
+        world.setResizable(false);
+        world.addMouseListener(this);
+        world.addKeyListener(this);
+        
+        String src = new File("").getAbsolutePath() + "/src/";
+        map = new JLabel(new ImageIcon(src + "Map.jpg"));
         
         setCursor(Toolkit.getDefaultToolkit().createCustomCursor(backgroundImage.getImage(), new Point(0, 0),
                 "custom cursor"));
-
-        Timer t = new Timer(16, this);
-        t.start();
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(this);
-        f.setVisible(true);
+        
+        Timer tim = new Timer(16, this);
+        tim.start();
+        world.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        world.add(map);
+        world.setLocationRelativeTo(null);
+        world.setVisible(true);
     }
 
     @Override
@@ -66,19 +134,8 @@ public class Main extends JPanel implements ActionListener, MouseListener, KeyLi
         // Call the paint method of the Map object to draw the background
         background.paint(g);
         g.setColor(Color.red);
-        
+//        g.drawOval((int)inputted.getLatitude(), (int)inputted.getLongitude(), 10, 10);
         double[] paraguay = convert(-32.815428, -56.094636);
-        
-        g.fillOval((int)paraguay[0], (int)paraguay[1], 10, 10);
-        g.fillOval((int)((1.601554 - minLong)*xScale-75), (int)(-(42.546245 + minLat)*yScale), 10, 10); //Andora
-        g.fillOval((int)((25.48583 - minLong)*xScale-75), (int)(-(42.733883 + minLat)*yScale), 10, 10); //Bulgaria
-        g.fillOval((int)((-95.712891 - minLong)*xScale-75), (int)(-(37.09024 + minLat)*yScale), 10, 10); //United States
-        g.fillOval((int)((-3.74922 - minLong)*xScale-75), (int)(-(40.463667 + minLat)*yScale), 10, 10); //Spain
-        g.fillOval((int)((-3.435973	 - minLong)*xScale-75), (int)(-(55.378051 + minLat)*yScale), 10, 10); //United Kingdom
-        g.fillOval((int)((28.233608 - minLong)*xScale-75), (int)(-(-29.609988	 + minLat)*yScale), 10, 10); //Lesotho
-        g.fillOval((int)((	-51.92528 - minLong)*xScale-75), (int)(-(-14.235004	 + minLat)*yScale), 10, 10); //Brazil
-        g.fillOval((int)((28.233608 + 180) * (width / 360)), ((int)((-29.609988 * -1) + 90) * (height / 180)), 10, 10);
-
     }
     
     public double[] convert(double lon, double lat){
@@ -89,34 +146,9 @@ public class Main extends JPanel implements ActionListener, MouseListener, KeyLi
         double[] res = {x,y};
         return res;
     }
-    
-    public double distance(double lat1, double lon1, double lat2, double lon2) {
-    	// The math module contains a function
-        // named toRadians which converts from
-        // degrees to radians.
-        lon1 = Math.toRadians(lon1);
-        lon2 = Math.toRadians(lon2);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
- 
-        // Haversine formula 
-        double dlon = lon2 - lon1; 
-        double dlat = lat2 - lat1;
-        double a = Math.pow(Math.sin(dlat / 2), 2)
-                 + Math.cos(lat1) * Math.cos(lat2)
-                 * Math.pow(Math.sin(dlon / 2),2);
-             
-        double c = 2 * Math.asin(Math.sqrt(a));
- 
-        // Radius of earth in kilometers. Use 3956 
-        // for miles
-        double r = 6371;
- 
-        // calculate the result
-        return(c * r);
-    } 
- 
 
+   
+    
     @Override
     public void mouseClicked(MouseEvent arg0) {
         // TODO Auto-generated method stub
@@ -140,15 +172,12 @@ public class Main extends JPanel implements ActionListener, MouseListener, KeyLi
 
     }
 
-    @Override
-    public void mouseReleased(MouseEvent arg0) {
-        // TODO Auto-generated method stub
-
-    }
 
     @Override
-    public void actionPerformed(ActionEvent arg0) {
-        repaint();
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button) {
+        	System.out.println(textField.getText());
+        }
     }
 
     @Override
@@ -163,6 +192,12 @@ public class Main extends JPanel implements ActionListener, MouseListener, KeyLi
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
