9519dc8ee059cdb9ed1d1036f42e157776a99fe1
Arapaimace <137009102+Arapaimace@users.noreply.github.com>
2024-05-07 09:22:57 -0700

    Implemented xy system, not using it yet

--- Maple/src/Coordinate.java
+++ Maple/src/Coordinate.java
@@ -2,13 +2,39 @@
 public class Coordinate {
 	private double latitude;
 	private double longitude;
+	private int x;
+	private int y;
 	
 	public Coordinate(double latitude, double longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
+	//to be implemented once we add all the x/y-values to the csv
+	public Coordinate(double latitude, double longitude, int x, int y){
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.x = x;
+		this.y = y;
+	}
 
-	public String toString(){
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public String toString() {
 		return "latitude: " + latitude + " longitude: " + longitude;
 	}
 }
