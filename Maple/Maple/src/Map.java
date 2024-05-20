5e6e30004748ee9c5e203afdbb6efda43bfaa197
Arapaimace <137009102+Arapaimace@users.noreply.github.com>
2024-05-20 11:36:01 -0700

    Add files via upload

---
+++ Maple/Maple/src/Map.java
@@ -0,0 +1,40 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+public class Map {
+	
+	//necessary code to paint the background for the game
+	private Image background;
+	private int x, y;
+	public Map() {
+	    background = getImage("Map.jpg");
+		x = 0;
+		y = 0;
+	}
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Map.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	public void paint(Graphics g) {
+	    Graphics2D g2 = (Graphics2D) g;
+	    g2.drawImage(background, x, y, null);
+	}
+
+
+
+}
